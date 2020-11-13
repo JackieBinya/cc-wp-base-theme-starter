8d14d929c3aa1b42805576dc47bf3ecaf18d29da
JackieBinya <jacquelinebinya@gmail.com>
2020-11-13 12:01:42 +0200

    Adds set up files

--- functions.php
+++ functions.php
@@ -1,7 +1,14 @@
 <?php
-	add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
+	add_action( 'wp_enqueue_scripts', 'enqueue_styles' );
 
-function enqueue_parent_styles() {
-				 wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+	 add_action( 'wp_enqueue_scripts', 'enqueue_scripts' );
+
+
+function enqueue_styles() {
+	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
+}
+
+function enqueue_scripts() {
+	wp_enqueue_script( 'vocabulary', get_template_directory_uri() . '/assets/js/vocabulary.js' );
 }
 
